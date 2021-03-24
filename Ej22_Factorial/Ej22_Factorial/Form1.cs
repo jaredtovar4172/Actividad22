f1ed7031744d41e7ea88af4b8ed8f57916597d10
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-03-23 21:21:03 -0600

    Add files via upload

---
+++ Ej22_Factorial/Ej22_Factorial/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej22_Factorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fact = Convert.ToInt32(textBox1.Text);
+            long factorial = 1; ;
+            for (int i = fact; i > 0; i--)
+            {
+                factorial = factorial * i;
+            }
+            textBox2.Text = factorial.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox1.Focus();
+            textBox2.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult salir;
+            salir = MessageBox.Show("Deseas salir de la aplicacion?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
